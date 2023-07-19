cca2e97d0802a7b58c3c16c839ede787767e605e
jovanjenjic <57072437+jovanjenjic@users.noreply.github.com>
2023-07-19 13:04:09 +0200

    First commit

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>React Lightweight Calendar</span>,
+  project: {
+    link: 'https://github.com/jovanjenjic/react-lightweight-calendar',
+  },
+  docsRepositoryBase: 'https://github.com/jovanjenjic/react-lightweight-calendar',
+  footer: {
+    text: 'React Lightweight Calendar',
+  },
+}
+
+export default config
