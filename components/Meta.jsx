2f30c6eaf8f91eb92a908053dca86313c8dd6b1a
jovanjenjic <57072437+jovanjenjic@users.noreply.github.com>
2023-07-20 20:24:26 +0200

    Smaller gif, connect to google analytics, meta icon

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -1,10 +1,69 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { pageview } from '../utils/analytics.js';
 
 const Meta = ({ description, title, keywords }) => {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+    // When the component is mounted, subscribe to router changes
+    // and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  console.log('process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS)
+  
   return (
     <Head>
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      {/* <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" /> */}
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta property="og:image" content="/favicon.png" />
       {title && <title>{title}</title>}
       {title && <meta property="og:title" content={title} />}
       {description && <meta name="description" content={description} />}
--- utils/analytics.js
+++ utils/analytics.js
@@ -1,10 +1,11 @@
-import ReactGA from 'react-ga';
-
-export const initGA = () => {
-  ReactGA.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS);
+// log the pageview with their URL
+export const pageview = (url) => {
+  window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+    page_path: url,
+  });
 };
 
-export const logPageView = () => {
-  ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
+// log specific events happening.
+export const event = ({ action, params }) => {
+  window.gtag("event", action, params);
 };
