c7b33c3940967ed424f384b59b7a651f3a84808d
Jovan Jenjic <jovanjenjic@Jovans-MacBook-Pro.local>
2024-06-07 11:17:24 +0200

    SEO changes jovan

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
 
-const Meta = ({ description, title, keywords }) => {
+const Meta = ({ description, title, keywords, canonical }) => {
   return (
     <Head>
-      {title && <title>{title}</title>}
-      {title && <meta property="og:title" content={title} />}
-      {description && <meta name="description" content={description} />}
-      {description && <meta property="og:description" content={description} />}
-      {keywords && <meta name="keywords" content={keywords} />}
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <meta http-equiv="Content-Language" content="en" />
+      <link rel="canonical" href={canonical} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
   );
 }
@@ -18,12 +21,14 @@ Meta.propTypes = {
   description: PropTypes.string,
   title: PropTypes.string,
   keywords: PropTypes.string,
+  canonical: PropTypes.string,
 };
 
 Meta.defaultProps = {
   description: null,
   title: null,
   keywords: null,
+  canonical: null,
 };
 
 export default Meta;
